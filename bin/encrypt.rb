769ac8d59971fc7a6d6f6a41af4204c5f095d159
Sebastian Abondano <sabondano1@gmail.com>
2015-06-03 17:53:39 -0600

    modified encrypt.rb to read message from file, encrypt, and write encrypted message to a new file

--- bin/encrypt.rb
+++ bin/encrypt.rb
@@ -2,6 +2,7 @@ require_relative '../lib/enigma/key_generator'
 require_relative '../lib/enigma/offset_calculator'
 require_relative '../lib/enigma/encryptor'
 require_relative '../lib/enigma/file_reader'
+require_relative '../lib/enigma/file_writer'
 
 message_file = ARGV[0]
 encryption_file = ARGV[1]
@@ -11,8 +12,7 @@ key.generate_key
 offset = OffsetCalculator.new(today, key)
 encryptor = Encryptor.new(offset.all_final_rotations)
 filereader = FileReader.new(message_file)
-puts encryptor.encrypt(filereader.read_message)
-
-
+filewriter = FileWriter.new(encryption_file)
+filewriter.write_encryption("#{encryptor.encrypt(filereader.read_message)}")
 
 puts "Created '#{encryption_file}' with the key #{key.number} and date #{today}"

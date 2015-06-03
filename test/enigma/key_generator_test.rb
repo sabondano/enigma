6c92d9ff9ac6f72799e5383735b08dfee03ed0ef
Sebastian Abondano <sabondano1@gmail.com>
2015-06-02 21:31:43 -0600

    reorganized what used to be an acceptance test into unit tests

--- test/enigma/key_generator_test.rb
+++ test/enigma/key_generator_test.rb
@@ -1,24 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../../lib/enigma/key_generator'
-require 'pry'
 
 class KeyGeneratorTest < Minitest::Test
-	def test_acceptance
-		key = KeyGenerator.new
-		key.generate_key
-
-		assert key.number.is_a?(Fixnum)
-		assert key.number.to_s.length == 5
-
-
-		key = KeyGenerator.new([4, 1, 5, 2, 1])
-
-		assert_equal 41, key.rotation("A")
-		assert_equal 15, key.rotation("B")
-		assert_equal 52, key.rotation("C")
-		assert_equal 21, key.rotation("D")
-	end
 
 	def test_it_generates_a_number
 		key = KeyGenerator.new
@@ -40,8 +24,31 @@ class KeyGeneratorTest < Minitest::Test
 		key_1.generate_key
 		key_2.generate_key
 
-
 		refute key_1.number == key_2.number
+	end
+
+	def test_rotation_A_returns_the_first_two_digits_of_the_key
+		key = KeyGenerator.new([4, 1, 5, 2, 1])
+
+		assert_equal 41, key.rotation("A")
+	end
+
+	def test_rotation_B_returns_the_second_and_third_digits_of_the_key
+		key = KeyGenerator.new([4, 1, 5, 2, 1])
+
+		assert_equal 15, key.rotation("B")
+	end
+
+	def test_rotation_C_returns_the_third_and_fourth_digits_of_the_key
+		key = KeyGenerator.new([4, 1, 5, 2, 1])
+
+		assert_equal 52, key.rotation("C")
+	end
+
+	def test_rotation_D_returns_the_fourth_and_fifth_digits_of_the_key
+		key = KeyGenerator.new([4, 1, 5, 2, 1])
+
+		assert_equal 21, key.rotation("D")
 	end
 
 end

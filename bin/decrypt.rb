a113a8363aad37a684ab1bf22734a1db9baa053b
Sebastian Abondano <sabondano1@gmail.com>
2015-06-03 19:21:29 -0600

    modified decrypt for it to return hidden messages in the command line

--- bin/decrypt.rb
+++ bin/decrypt.rb
@@ -1,11 +1,12 @@
 require_relative '../lib/enigma/key_generator'
 require_relative '../lib/enigma/offset_calculator'
-require_relative '../lib/enigma/encryptor'
+require_relative '../lib/enigma/decryptor'
 require_relative '../lib/enigma/file_reader'
 
 encrypted_file = ARGV[0]
 date = ARGV[1]
 key = ARGV[2]
+key = KeyGenerator.new(key.split(""))
 offset = OffsetCalculator.new(date, key)
 decryptor = Decryptor.new(offset.all_final_rotations)
 filereader = FileReader.new(encrypted_file)

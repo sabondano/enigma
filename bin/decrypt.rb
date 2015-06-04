5be6cd2a9bb3a8d269e7be46f2cfe15174a710a5
Sebastian Abondano <sabondano1@gmail.com>
2015-06-03 20:50:25 -0600

    modified decrypt.rb file for it to write the decrypted message to a file

--- bin/decrypt.rb
+++ bin/decrypt.rb
@@ -2,14 +2,19 @@ require_relative '../lib/enigma/key_generator'
 require_relative '../lib/enigma/offset_calculator'
 require_relative '../lib/enigma/decryptor'
 require_relative '../lib/enigma/file_reader'
+require_relative '../lib/enigma/file_writer'
 
 encrypted_file = ARGV[0]
-date = ARGV[1]
+decrypted_file_name = ARGV[1]
 key = ARGV[2]
+date = ARGV[3]
 key = KeyGenerator.new(key.split(""))
 offset = OffsetCalculator.new(date, key)
 decryptor = Decryptor.new(offset.all_final_rotations)
-filereader = FileReader.new(encrypted_file)
-cracked_message = decryptor.decrypt(filereader.read_message)
+file_reader = FileReader.new(encrypted_file)
+file_writer = FileWriter.new(decrypted_file_name)
+file_writer.write_encryption("#{decryptor.decrypt(file_reader.read_message)}")
+cracked_message = decryptor.decrypt(file_reader.read_message)
 
+puts "Created '#{decrypted_file_name}' with the key #{key.number} and date #{date}"
 puts "The hidden message is: #{cracked_message}"
